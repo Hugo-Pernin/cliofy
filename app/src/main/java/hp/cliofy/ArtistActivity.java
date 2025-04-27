6652d8178a359091c01ad1ad315c52a0ccd3ced1
Hugo-Pernin <hugopernin71@gmail.com>
2025-04-27 14:29:04 +0200

    Ajout d'informations à la ArtistActivity

--- app/src/main/java/hp/cliofy/ArtistActivity.java
+++ app/src/main/java/hp/cliofy/ArtistActivity.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.WindowManager;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import java.util.List;
+
 import hp.cliofy.DAO.GeneralDAO;
+import hp.cliofy.Item.Album;
 import hp.cliofy.Item.Artist;
+import hp.cliofy.Item.Track;
 
 public class ArtistActivity extends AppCompatActivity {
     private Artist artist;
 
     private GeneralDAO generalDAO;
+
+    private TextView informations;
+
+    private ListView topTracksListView;
+    private List<Track> topTracksList;
+
+    private ListView albumsListView;
+    private List<Album> albumsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +43,8 @@ public class ArtistActivity extends AppCompatActivity {
 
         generalDAO = GeneralDAO.getInstance();
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(artist.toString() + "\n" +
+        informations = findViewById(R.id.informations);
+        informations.setText(artist.toString() + "\n" +
                 artist.getFollowersTotal() + " followers\n" +
                 "Genres: " + artist.getGenres());
 
