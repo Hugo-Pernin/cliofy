548d54729c7248d447da54032a493af9f2cecd19
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-03 17:44:03 +0200

    Délocalisation des requêtes PUT dans la classe ApiClient

--- app/src/main/java/hp/cliofy/Model/Service/ApiClient.java
+++ app/src/main/java/hp/cliofy/Model/Service/ApiClient.java
@@ -2,11 +2,16 @@ package hp.cliofy.Model.Service;
 
 import org.json.JSONObject;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -70,6 +75,60 @@ public class ApiClient {
                     urlConnection = (HttpURLConnection) url.openConnection();
                     urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
                     urlConnection.setRequestMethod("GET");
+
+                    int code = urlConnection.getResponseCode();
+                    if (code !=  200) {
+                        throw new IOException("Invalid response from server: " + code);
+                    }
+
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    json[0] = new JSONObject(rd.readLine());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return json[0];
+    }
+
+    public static JSONObject putRequest(String endpoint, String data) {
+        final JSONObject[] json = {null}; // A one-entry array is necessary
+
+        Thread thread = new Thread(() -> {
+            try {
+                HttpURLConnection urlConnection = null;
+                try {
+                    URL url = new URL(endpoint);
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.setRequestMethod("PUT");
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setDoInput(true);
+                    urlConnection.setChunkedStreamingMode(0);
+
+                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+                    writer.write(data);
+                    writer.flush();
+                    writer.close();
+                    out.close();
 
                     int code = urlConnection.getResponseCode();
                     if (code !=  200) {
--- app/src/main/java/hp/cliofy/Model/Service/PlayerService/PlayerService.java
+++ app/src/main/java/hp/cliofy/Model/Service/PlayerService/PlayerService.java
@@ -1,17 +1,6 @@
 package hp.cliofy.Model.Service.PlayerService;
 
-import org.json.JSONObject;
-
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import hp.cliofy.Model.Service.ApiClient;
 
 public class PlayerService implements IPlayerService {
     private final String PATH = "https://api.spotify.com/v1/me/player/";
@@ -23,54 +12,8 @@ public class PlayerService implements IPlayerService {
 
     @Override
     public void playWithOffset(String uri, int offset) {
-        Thread thread = new Thread(() -> {
-            try {
-                HttpURLConnection urlConnection = null;
-                try {
-                    String putData = "{\"context_uri\":\"" + uri + "\",\"offset\":{\"position\":" + offset + "}}";
-
-                    URL url = new URL(PATH + "play");
-
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestProperty("Authorization", "Bearer " + accessToken);
-                    urlConnection.setRequestProperty("Content-Type", "application/json");
-                    urlConnection.setRequestMethod("PUT");
-                    urlConnection.setDoOutput(true);
-                    urlConnection.setDoInput(true);
-                    urlConnection.setChunkedStreamingMode(0);
-
-                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
-                    writer.write(putData);
-                    writer.flush();
-                    writer.close();
-                    out.close();
-
-                    int code = urlConnection.getResponseCode();
-                    if (code !=  200) {
-                        throw new IOException("Invalid response from server: " + code);
-                    }
-
-                    BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    JSONObject json = new JSONObject(rd.readLine());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (urlConnection != null) {
-                        urlConnection.disconnect();
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        thread.start();
-
-        try {
-            thread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        String endpoint = PATH + "play";
+        String putData = "{\"context_uri\":\"" + uri + "\",\"offset\":{\"position\":" + offset + "}}";
+        ApiClient.putRequest(endpoint, putData);
     }
 }
