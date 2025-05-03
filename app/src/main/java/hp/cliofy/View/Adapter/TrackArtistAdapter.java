9999fb19eeac5b87b92273bf600db5f2d8b8aa91
Hugo-Pernin <hugopernin71@gmail.com>
2025-05-03 16:13:48 +0200

    Implémentation du bouton album des top tracks de l'ArtistActivity

--- app/src/main/java/hp/cliofy/View/Adapter/TrackArtistAdapter.java
+++ app/src/main/java/hp/cliofy/View/Adapter/TrackArtistAdapter.java
@@ -1,6 +1,7 @@
 package hp.cliofy.View.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 
 import java.util.List;
 
+import hp.cliofy.Model.Item.Album;
 import hp.cliofy.Model.Item.Track;
 import hp.cliofy.Model.Service.FacadeService;
 import hp.cliofy.R;
+import hp.cliofy.View.Activity.AlbumActivity;
 
 public class TrackArtistAdapter extends ArrayAdapter<Track> {
     public TrackArtistAdapter(Context context, List<Track> data) {
@@ -45,11 +49,11 @@ public class TrackArtistAdapter extends ArrayAdapter<Track> {
         });
         albumButton.setOnClickListener((View view) -> {
             // TODO respecter S & O
-            /*Intent intent = new Intent(getContext(), AlbumActivity.class);
+            Intent intent = new Intent(getContext(), AlbumActivity.class);
             Album album = track.getAlbum();
             Gson gson = new Gson();
             intent.putExtra("album", gson.toJson(album));
-            getContext().startActivity(intent);*/
+            getContext().startActivity(intent);
         });
 
         return convertView;
